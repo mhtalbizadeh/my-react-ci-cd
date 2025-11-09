a9f7cc773a5009f8099ac878d2264ac52314059f
mohammad hossein <69706705+mhtalbizadeh@users.noreply.github.com>
2025-11-09 13:33:28 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/my-react-ci-cd/',  // 👈 این خط اضافه یا اصلاح بشه
 })
